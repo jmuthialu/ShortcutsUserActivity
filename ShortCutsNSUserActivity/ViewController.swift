7bfe057c15f13157229c43c17ecfde81e07999f6
Jay Muthialu <jay.muthialu@turner.com>
2018-08-13 11:15:35 -0400

    Initial creation of demo app

--- ShortCutsNSUserActivity/ViewController.swift
+++ ShortCutsNSUserActivity/ViewController.swift
@@ -12,9 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        donateUserActivity()
     }
 
+    func donateUserActivity() {
+        let activity = NSUserActivity(activityType: "com.glintpod.ShortCutsNSUserActivity123.Simple")
+        activity.title = "Simple"
+        activity.userInfo = ["simple": "simpleTest"]
+        activity.isEligibleForPrediction = true
+        activity.isEligibleForSearch = true
+        activity.persistentIdentifier = "com.glintpod.ShortCutsNSUserActivity123.Simple"
+        view.userActivity = activity
+        activity.becomeCurrent()
+    }
+    
+    func testAlert() {
+        let alert = UIAlertController(title: "Test Alert", message: "Shortcuts test", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true)
+    }
 
 }
 
